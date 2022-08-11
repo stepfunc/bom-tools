79ff8de2622e103ea8a8cfdd305e954d80cc0eec
jadamcrain <jadamcrain@gmail.com>
2022-08-11 13:14:34 -0700

    produce detailed license reports

---
+++ src/config.rs
@@ -0,0 +1,156 @@
+use serde::{Deserialize, Serialize};
+use std::collections::{BTreeMap, BTreeSet};
+
+/// A copyright statement associated with a license
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Copyright {
+    /// Copyright statement is present in the license file that consists of one of more lines
+    Lines(Vec<String>),
+    /// No copyright statement is present in the license file
+    NotPresent,
+}
+
+impl Copyright {
+    fn lines(&self) -> Vec<String> {
+        match self {
+            Copyright::Lines(x) => x.clone(),
+            Copyright::NotPresent => vec!["No copyright statement was provided by the author even though they license may refer to it".to_string()],
+        }
+    }
+}
+
+/// Where information about the crate can be found
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Source {
+    /// This crate came from crates.io
+    #[serde(rename = "crates.io")]
+    CratesIo,
+}
+
+pub(crate) struct LicenseInfo {
+    pub(crate) url: &'static str,
+    pub(crate) text: &'static str,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) enum License {
+    Unknown,
+    #[serde(rename = "ISC")]
+    Isc {
+        copyright: Copyright,
+    },
+    #[serde(rename = "MIT")]
+    Mit {
+        copyright: Copyright,
+    },
+    /// Openssl / SSLeay license (https://www.openssl.org/source/license-openssl-ssleay.txt)
+    #[serde(rename = "OpenSSL")]
+    OpenSsl,
+    /// Boost software license v1 (https://www.boost.org/users/license.html)
+    #[serde(rename = "BSLv1")]
+    Bsl1,
+    /// MPL Version 2.0 (https://www.mozilla.org/en-US/MPL/2.0/)
+    #[serde(rename = "MPLv2")]
+    Mpl2,
+    /// 3-clause BSD (https://opensource.org/licenses/BSD-3-Clause)
+    #[serde(rename = "BSD3")]
+    Bsd3 {
+        copyright: Copyright,
+    },
+    /// Unicode License Agreement - Data Files and Software (2016)
+    #[serde(rename = "UnicodeDFS2016")]
+    UnicodeDfs2016,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct Package {
+    /// id of the allowed package
+    pub(crate) id: String,
+    /// Where the package came from
+    pub(crate) source: Source,
+    /// license identification
+    pub(crate) licenses: Vec<License>,
+}
+
+impl Package {
+    pub(crate) fn url(&self) -> String {
+        match self.source {
+            Source::CratesIo => format!("https://crates.io/crates/{}", self.id),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct Config {
+    /// packages that are build-only dependencies, are not linked/distributed, and are ignored in the build log
+    pub(crate) build_only: BTreeSet<String>,
+    /// packages that are licensed by the vendor and are distributed under a custom license
+    pub(crate) vendor: BTreeSet<String>,
+    /// 3rd party packages that are allowed to be build dependencies
+    pub(crate) third_party: BTreeMap<String, Package>,
+}
+
+impl License {
+    pub(crate) fn info(&self) -> LicenseInfo {
+        LicenseInfo {
+            url: self.url(),
+            text: self.text(),
+        }
+    }
+
+    pub(crate) fn copyright(&self) -> Option<Vec<String>> {
+        match self {
+            License::Unknown => None,
+            License::Isc { copyright } => Some(copyright.lines()),
+            License::Mit { copyright } => Some(copyright.lines()),
+            License::OpenSsl => None,
+            License::Bsl1 => None,
+            License::Mpl2 => None,
+            License::Bsd3 { copyright } => Some(copyright.lines()),
+            License::UnicodeDfs2016 => None,
+        }
+    }
+
+    pub(crate) fn text(&self) -> &'static str {
+        match self {
+            License::Isc { .. } => std::include_str!("../licenses/isc.txt"),
+            License::Mit { .. } => std::include_str!("../licenses/mit.txt"),
+            License::OpenSsl => std::include_str!("../licenses/openssl.txt"),
+            License::Bsl1 => std::include_str!("../licenses/bsl.txt"),
+            License::Mpl2 => std::include_str!("../licenses/mpl2.txt"),
+            License::Bsd3 { .. } => std::include_str!("../licenses/bsd3.txt"),
+            License::UnicodeDfs2016 => std::include_str!("../licenses/unicode_dfs_2016.txt"),
+            License::Unknown => panic!("You must define unknown licenses"),
+        }
+    }
+
+    pub(crate) fn spdx_short(&self) -> &'static str {
+        match self {
+            License::Isc { .. } => "ISC",
+            License::Mit { .. } => "MIT",
+            License::OpenSsl => "OpenSSL",
+            License::Bsl1 => "BSL-1.0",
+            License::Mpl2 => "MPL-2.0",
+            License::Bsd3 { .. } => "BSD-3-Clause",
+            License::UnicodeDfs2016 => "Unicode-DFS-2016",
+            License::Unknown => {
+                panic!("You must define unknown licenses")
+            }
+        }
+    }
+
+    pub(crate) fn url(&self) -> &'static str {
+        match self {
+            License::Isc { .. } => "https://spdx.org/licenses/ISC.html",
+            License::Mit { .. } => "https://spdx.org/licenses/MIT.html",
+            License::OpenSsl => "https://spdx.org/licenses/OpenSSL.html",
+            License::Bsl1 => "https://spdx.org/licenses/BSL-1.0.html",
+            License::Mpl2 => "https://spdx.org/licenses/MPL-2.0.html",
+            License::Bsd3 { .. } => "https://spdx.org/licenses/BSD-3-Clause.html",
+            License::UnicodeDfs2016 => "https://spdx.org/licenses/Unicode-DFS-2016.html",
+            License::Unknown => {
+                panic!("You must define unknown licenses")
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+extern crate core;
+
+use crate::config::{Config, LicenseInfo, Package, Source};
 use clap::{Parser, Subcommand};
+use std::collections::BTreeMap;
 use std::fs::File;
 use std::path::Path;
 
@@ -24,6 +28,18 @@ enum Commands {
         #[clap(value_parser)]
         path: std::path::PathBuf,
     },
+    /// generates the skeleton of a configuration using the build log and the output of cargo tree
+    GenConfig {
+        /// path to the log file
+        #[clap(value_parser)]
+        log_path: std::path::PathBuf,
+        /// path to the output of cargo tree
+        #[clap(value_parser)]
+        tree_path: std::path::PathBuf,
+        /// path to the output config file
+        #[clap(value_parser)]
+        output_path: std::path::PathBuf,
+    },
     /// reports the differences between a log file and the contents of cargo tree
     DiffTree {
         /// path to the log file
@@ -33,8 +49,19 @@ enum Commands {
         #[clap(value_parser)]
         tree_path: std::path::PathBuf,
     },
+    /// outputs a human readable report of all 3rd party licenses
+    GenLicenses {
+        /// path to the log file
+        #[clap(value_parser)]
+        log_path: std::path::PathBuf,
+        /// path to the JSON configuration file
+        #[clap(value_parser)]
+        config_path: std::path::PathBuf,
+    },
 }
 
+/// json configuration structures
+pub(crate) mod config;
 /// read cargo log files for dependency information
 pub(crate) mod log;
 /// parse the output of cargo tree
@@ -50,6 +77,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             log_path,
             tree_path,
         } => diff_tree(&log_path, &tree_path),
+        Commands::GenConfig {
+            log_path,
+            tree_path,
+            output_path,
+        } => generate_config(&log_path, &tree_path, &output_path),
+        Commands::GenLicenses {
+            log_path,
+            config_path,
+        } => gen_licenses(&log_path, &config_path),
     }
 }
 
@@ -67,6 +103,47 @@ fn print_log(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     for (id, usage) in packages {
         println!("{} {}", id, usage.versions)
     }
+    Ok(())
+}
+
+fn generate_config(
+    log_path: &Path,
+    tree_path: &Path,
+    output_path: &Path,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let packages = log::read_packages(log_path)?;
+    let tree = tree::parse_tree(File::open(tree_path)?)?;
+
+    let mut config = Config {
+        build_only: Default::default(),
+        vendor: Default::default(),
+        third_party: Default::default(),
+    };
+
+    // then tell us what's in log that isn't in the tree
+    for (id, usage) in packages.iter() {
+        for version in usage.versions.values() {
+            if tree
+                .iter()
+                .any(|dep| &dep.id == id && &dep.version == version)
+            {
+                config.third_party.insert(
+                    id.clone(),
+                    Package {
+                        id: id.clone(),
+                        source: Source::CratesIo,
+                        licenses: Vec::new(),
+                    },
+                );
+            } else {
+                // it's a build only dependency
+                config.build_only.insert(id.clone());
+            }
+        }
+    }
+
+    let writer = std::io::BufWriter::new(File::create(output_path)?);
+    serde_json::to_writer_pretty(writer, &config)?;
     Ok(())
 }
 
@@ -107,6 +184,74 @@ fn diff_tree(log_path: &Path, tree_path: &Path) -> Result<(), Box<dyn std::error
                 );
             }
         }
+    }
+
+    Ok(())
+}
+
+fn gen_licenses(log_path: &Path, config_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let mut log = log::read_packages(log_path)?;
+    let config: Config = serde_json::from_reader(File::open(config_path)?)?;
+
+    // remove build-only and vendor dependencies
+    log.retain(|id, _| !(config.build_only.contains(id) || config.vendor.contains(id)));
+
+    // first summarize the licenses
+    let mut licenses: BTreeMap<&'static str, LicenseInfo> = BTreeMap::new();
+    for (id, _) in log.iter() {
+        let pkg = config
+            .third_party
+            .get(id)
+            .ok_or_else(|| format!("3rd party package {} not in the allow list", id))?;
+        for license in pkg.licenses.iter() {
+            licenses.insert(license.spdx_short(), license.info());
+        }
+    }
+
+    println!("This binary contains open source dependencies under the following licenses:");
+    println!();
+    for (spdx, info) in licenses.iter() {
+        println!("  * {}", spdx);
+        println!("      - {}", info.url);
+    }
+    println!();
+    println!("Copies of these licenses are provided at the end of this document. They may also be obtained from the URLs above.");
+    println!();
+
+    for id in log.keys() {
+        let pkg = config
+            .third_party
+            .get(id)
+            .ok_or_else(|| format!("3rd party package {} not in the allow list", id))?;
+        println!("crate: {}", pkg.id);
+        println!("url: {}", pkg.url());
+
+        if pkg.licenses.is_empty() {
+            return Err(format!("No license specified for {}", id).into());
+        }
+
+        let licenses: Vec<String> = pkg
+            .licenses
+            .iter()
+            .map(|x| x.spdx_short().to_string())
+            .collect();
+        println!("licenses: {}", licenses.join(" AND "));
+
+        // write out copyright statements
+        for lic in pkg.licenses.iter() {
+            if let Some(lines) = lic.copyright() {
+                for line in lines {
+                    println!("{}", line);
+                }
+            }
+        }
+
+        println!();
+    }
+
+    for info in licenses.values() {
+        println!("{}", info.text);
+        println!();
     }
 
     Ok(())
